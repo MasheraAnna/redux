f3c86344d41041942d4bf3591b3fedb166eb70c0
MasheraAnna <anna.mashera@gmail.com>
2017-01-02 23:15:03 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log(this.props.testStore);
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <input type = 'text' />
+          <button id = 'addTrack'> Add track </button>
+          <ul class = 'list'>
+          </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({
+    testStore: state
+  }),
+  dispatch =>({})
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css';
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-function playlist(state = [], action){
+
+const initialState = [
+	"Smells like teen spirit",
+	"Enter Sandman"
+	];
+
+function playlist(state = initialState, action){
 	if (action.type === 'ADD_TRACK'){
 		return [
 		...state,
@@ -20,17 +20,41 @@ function playlist(state = [], action){
 	}
 	return state;
 }
+
 const store = createStore(playlist);
 
-store.subscribe(()=>{
-	console.log('subscribe', store.getState());
-})
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
-store.dispatch({type: "ADD_TRACK", payload: 'Smels like spirit'});
-store.dispatch({type: "ADD_TRACK", payload: 'Enter Sandman'});
+///////////////////////////////////////////////
 
-const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-addTrackBtn.addEventListener('click', ()=>{
-	const trackName = document.querySekectorAll('.trackInput')[0].value;
-	console.log('trackname', trackName);
-})
+// import { createStore } from 'redux';
+
+
+
+
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+
+// store.subscribe(()=>{
+
+// 	list.innerHTML = '';
+// 	trackInput.value = '';
+// 	store.getState().forEach(track => {
+// 		const li = document.createElement('li');
+// 		li.textContent = track;
+// 		list.appendChild(li);
+// 	})
+
+// })
+
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// addTrackBtn.addEventListener('click', ()=>{
+// 	const trackName = trackInput.value;
+// 	store.dispatch({type: "ADD_TRACK", payload: trackName});
+// })
+
